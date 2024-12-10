70cc83662576b2d005a3a5603eca2cd52bed7657
Laura Purcell-Gates <laurapg1214@users.noreply.github.com>
2024-12-10 16:18:10 +0000

    homepage route set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'jobs#index'
+  root 'homepage#index'
 
   # JOBS
   get '/jobs'     => 'jobs#index'
   post '/jobs'    => 'jobs#create'
   get '/jobs/:id' => 'jobs#show'
+
+  # Redirect all other paths to index page, which will be taken over by AngularJS
+  get '*path' => 'homepage#index'
 end
