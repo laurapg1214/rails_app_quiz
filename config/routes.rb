73bfb5f90b7b61fca5432340cab7b849ff94cddf
Laura Purcell-Gates <laurapg1214@users.noreply.github.com>
2024-12-10 16:11:23 +0000

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'jobs#index'
+
+  # JOBS
+  get '/jobs'     => 'jobs#index'
+  post '/jobs'    => 'jobs#create'
+  get '/jobs/:id' => 'jobs#show'
 end
